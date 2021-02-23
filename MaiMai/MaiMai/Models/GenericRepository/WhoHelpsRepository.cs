35234d81519476259f531e2c85b382423e490d4b
Michael0302 <65208068+Michael0302@users.noreply.github.com>
2021-02-23 19:01:20 +0800

    Repository add

---
+++ MaiMai/MaiMai/Models/GenericRepository/WhoHelpsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WhosHelp.Models.GenericRepository;
+
+
+/// <summary>
+///  介面實作  CRUD
+/// </summary>
+namespace WhosHelp.Models
+{
+    public class WhoHelpsRepository<Table> : IRepository<Table> where Table : class
+    {
+        WhoHelpsEntities whdb = null;
+        DbSet<Table> dbTable = null;
+
+        public WhoHelpsRepository()
+        {
+            whdb = new WhoHelpsEntities();
+            dbTable = whdb.Set<Table>();
+        }
+        public void Create(Table _entity)
+        {
+            dbTable.Add(_entity);
+            whdb.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            dbTable.Remove(GetbyID(id));
+            whdb.SaveChanges();
+        }
+
+        public IEnumerable<Table> GetAll()
+        {
+            return dbTable;
+        }
+
+        public Table GetbyID(int id)
+        {
+            return dbTable.Find(id);
+        }
+
+        public void Update(Table _entity)
+        {
+            whdb.Entry<Table>(_entity).State = EntityState.Modified;
+            whdb.SaveChanges();
+        }
+    }
+}
