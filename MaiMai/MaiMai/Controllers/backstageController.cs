a9c988987e76bd0e8ae3ed5670387f0ba0607334
Michael0302 <65208068+Michael0302@users.noreply.github.com>
2021-02-24 16:49:23 +0800

    test add

---
+++ MaiMai/MaiMai/Controllers/backstageController.cs
@@ -0,0 +1,30 @@
+﻿using MaiMai.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class backstageController : Controller
+    {
+        // GET: backstage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        maimaiRepository<Member> mb = new maimaiRepository<Member>();
+        public ActionResult backstageIndex()
+        {
+            return View();
+        }
+
+        public ActionResult getMemberList_P()
+        {
+            var memList = mb.GetAll();
+            return Json(memList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
