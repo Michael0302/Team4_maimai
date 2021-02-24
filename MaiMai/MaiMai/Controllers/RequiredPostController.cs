7c9d0fa5e139b42adaf1fa01079d1d7e7cb2662a
Chia-wen Hsu <77603509+Chia-WenHsu@users.noreply.github.com>
2021-02-24 16:09:06 +0800

    卡在SQL

---
+++ MaiMai/MaiMai/Controllers/RequiredPostController.cs
@@ -0,0 +1,35 @@
+﻿using DocumentFormat.OpenXml.Bibliography;
+using MaiMai.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class RequiredPostController : Controller
+    {
+        // GET: RequiredPost
+
+        maimaiRepository<RequiredPost> requiredPostTable = new maimaiRepository<RequiredPost>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult requiredPostIndex() {
+
+            return View();
+        }
+        [HttpPost]
+        public JsonResult allrequiredPost() {
+            maimaiEntities db = new maimaiEntities();
+
+            var table = db.RequiredPost;
+
+            //var table = requiredPostTable.GetAll();
+          
+            return Json(table, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
