ac54ff7dfd99bb68d3b409c68957b68b482ef63f
Chia-wen Hsu <77603509+Chia-WenHsu@users.noreply.github.com>
2021-02-25 11:58:43 +0800

    showALlrequiredPost 成功

--- MaiMai/MaiMai/Controllers/RequiredPostController.cs
+++ MaiMai/MaiMai/Controllers/RequiredPostController.cs
@@ -1,5 +1,6 @@
 ﻿using DocumentFormat.OpenXml.Bibliography;
 using MaiMai.Models;
+using MaiMai.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,23 +14,70 @@ namespace MaiMai.Controllers
         // GET: RequiredPost
 
         maimaiRepository<RequiredPost> requiredPostTable = new maimaiRepository<RequiredPost>();
+        maimaiEntities db = new maimaiEntities();
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult requiredPostIndex() {
+        public ActionResult requiredPostIndex()
+        {
 
             return View();
         }
-        [HttpPost]
-        public JsonResult allrequiredPost() {
-            maimaiEntities db = new maimaiEntities();
 
-            var table = db.RequiredPost;
+       
+        public JsonResult allrequiredPost()
+        {
 
-            //var table = requiredPostTable.GetAll();
-          
+            var table = db.RequiredPost.Select(m => new RequiredPostViewModel_C()
+            {
+                RequiredPostID = m.RequiredPostID,
+                postTime = m.postTime,
+                postDescription = m.postDescription,
+                postName = m.postName,
+                postImg = m.postImg,
+                UserID = m.UserID,
+                requiredQTY = m.requiredQTY,
+                estimatePrice = m.estimatePrice,
+                TagID = m.TagID,
+                OrderID = m.OrderID,
+                userAccount = m.Member.userAccount
+            }).ToList();
+         
+
             return Json(table, JsonRequestBehavior.AllowGet);
+        }// allpostend
+
+        public ActionResult allpoccessPost() {
+
+            var table = db.RequiredPost.Where( m => m.OrderID != null).Select(m => new RequiredPostViewModel_C()
+            {
+                RequiredPostID = m.RequiredPostID,
+                postTime = m.postTime,
+                postDescription = m.postDescription,
+                postName = m.postName,
+                postImg = m.postImg,
+                UserID = m.UserID,
+                requiredQTY = m.requiredQTY,
+                estimatePrice = m.estimatePrice,
+                TagID = m.TagID,
+                OrderID = m.OrderID,
+                userAccount = m.Member.userAccount
+            }).ToList();
+
+
+            return Json(table, JsonRequestBehavior.AllowGet);
+
         }
-    }
-}
+
+
+        public ActionResult commemtProductPost() { 
+        
+            
+        }
+
+
+
+
+    }//class end
+}//namespace end
