7395ef00b4200fa58d58cf3b1a847057b4233b63
hcn0108 <77559249+hcn0108@users.noreply.github.com>
2021-02-26 17:10:22 +0800

    Login SIgnUP  Cookie

---
+++ MaiMai/MaiMai/Controllers/PersonalMarketController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class PersonalMarketController : Controller
+    {
+        // GET: PersonalMarket
+        public ActionResult Personal_Index()
+        {
+            return View();
+        }
+        
+    }
+}
