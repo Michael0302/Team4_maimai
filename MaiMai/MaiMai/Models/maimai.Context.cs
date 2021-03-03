d37fc3d59731c8b1e2a071da1feaf7fc2f4c28a3
Michael0302 <a199430209@gmail.com>
2021-03-03 22:39:43 +0800

    SQL server

--- MaiMai/MaiMai/Models/maimai.Context.cs
+++ MaiMai/MaiMai/Models/maimai.Context.cs
@@ -30,8 +30,8 @@ namespace MaiMai.Models
         public virtual DbSet<Member> Member { get; set; }
         public virtual DbSet<Order> Order { get; set; }
         public virtual DbSet<OrderDetail> OrderDetail { get; set; }
+        public virtual DbSet<ProductPost> ProductPost { get; set; }
         public virtual DbSet<RequiredPost> RequiredPost { get; set; }
         public virtual DbSet<Tag> Tag { get; set; }
-        public virtual DbSet<ProductPost> ProductPost { get; set; }
     }
 }
