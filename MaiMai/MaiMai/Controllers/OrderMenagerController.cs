74d6c410baef2114a1d69a156c2e9cf690816bc4
Chia-WenHsu <77603509+Chia-WenHsu@users.noreply.github.com>
2021-03-06 12:17:50 +0800

    建立訂單管理頁面

---
+++ MaiMai/MaiMai/Controllers/OrderMenagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class OrderMenagerController : Controller
+    {
+        // GET: OrderMenager
+        public ActionResult OrderMenagerIndex()
+        {
+            return View();
+        }
+    }
+}
