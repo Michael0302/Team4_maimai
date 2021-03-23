478a6619ab2af33646f31f05f31483930bfa1dec
MichaelXiao0302 <a199430209@gmail.com>
2021-03-23 20:59:57 +0800

    通知欄 bug

--- MaiMai/MaiMai/Models/Notification.cs
+++ MaiMai/MaiMai/Models/Notification.cs
@@ -20,5 +20,6 @@ namespace MaiMai.Models
         public string NotifyText { get; set; }
         public Nullable<System.DateTime> CreateTime { get; set; }
         public Nullable<bool> Status { get; set; }
+        public string Category { get; set; }
     }
 }
