a95c51d225d6b49d1aae3e39ebf06eb04fa6de41
Chia-wen Hsu <77603509+Chia-WenHsu@users.noreply.github.com>
2021-03-23 10:41:53 +0800

    payment

---
+++ MaiMai拷貝/MaiMai/ChatHubs/chatHub.cs
@@ -0,0 +1,147 @@
+﻿//using Microsoft.AspNet.SignalR;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+
+//namespace SignalRMvc.chatHubs
+//{
+//    public class chatHub : Hub
+//    {
+//        public void Sendmessage(string name, string message)
+//        {
+//            // Clients.All.hello();
+//            Clients.All.receiveMessage(name, message);
+//            //用戶調用客戶端的函數
+//        }
+//    }
+//}
+
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MaiMai.Models;
+using Microsoft.AspNet.SignalR;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace SignalRMvc.chatHubs
+{
+
+
+    public class chatHub : Hub
+    {
+        //public static List<User> users = new List<User>();
+
+        
+        maimaiEntities db = new maimaiEntities();
+
+        public void Group(int UserID)
+        {
+            var user = db.Member.Find(UserID);
+                user.connectionID = Context.ConnectionId;
+                db.SaveChanges();
+            
+
+            //判斷會員等級 管理員&優質會員>加入 VIP群組
+            if (user.userLevel != 3)
+            {
+                var reload_user = db.Member.Find(UserID);
+                Groups.Add(reload_user.connectionID, "VIP");
+            }
+        }
+        public override Task OnConnected()
+        {
+            //var user = users.Where(u => u.ConnectionID == Context.ConnectionId).SingleOrDefault();
+            //判断用户是否存在，否则添加集合
+            //if (user == null)
+            //{
+            //    user = new User("",  Context.ConnectionId);
+            //    users.Add(user);
+            //}
+            return base.OnConnected();
+        }
+
+        public void Send(int sender,  string message)
+        {
+            // 呼叫所有客戶端的sendMessage方法
+            Clients.All.addMessage(message);
+
+            Notification noti = new Notification() {
+                SenderID = sender,
+                ReciverLevel = "All",
+                NotifyText = message,
+                CreateTime = DateTime.Now
+            };
+
+            db.Notification.Add(noti);
+            db.SaveChanges();
+        }
+
+        public void SendToOne(int sender, int reciver, string message)
+        {
+            var user = db.Member.Find(reciver);
+            if(user != null)
+            {                
+                Clients.Client(user.connectionID).addMessage(message);
+
+                Notification noti = new Notification()
+                {
+                    SenderID = sender,
+                    ReciverLevel = reciver.ToString(),
+                    NotifyText = message,
+                    CreateTime = DateTime.Now
+                };
+
+                db.Notification.Add(noti);
+                db.SaveChanges();
+            }
+        }
+
+        public void OneToOneChat(int sender, int reciver, string message)
+        {
+            var user = db.Member.Find(reciver);
+            if (user != null)
+            {
+                Clients.Client(user.connectionID).reciverMessage(message, new {
+                    user.UserID,
+                    user.userAccount
+                    });
+                Clients.Caller.senderMessage(message);
+
+                Chat chat = new Chat()
+                {
+                    SenderID = sender,
+                    ReciverID = reciver,
+                    ChatText = message,
+                    ChatTime = DateTime.Now
+                };
+
+                db.Chat.Add(chat);
+                db.SaveChanges();
+            }
+        }
+
+        public void SendToVIP(int sender,  string message)
+        {
+            Clients.Group("VIP").addMessage(message);
+           
+            Notification noti = new Notification()
+            {
+                SenderID = sender,
+                ReciverLevel = "VIP",
+                NotifyText = message,
+                CreateTime = DateTime.Now
+            };
+
+            db.Notification.Add(noti);
+            db.SaveChanges();
+        }
+
+
+
+    }
+}
---
+++ MaiMai拷貝/MaiMai/Controllers/MembersController.cs
@@ -0,0 +1,154 @@
+﻿using MaiMai.Models;
+using MaiMai.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaiMai.Controllers
+{
+    public class MembersController : Controller
+    {
+        // GET: Members
+        maimaiEntities db = new maimaiEntities();
+        [HttpPost]
+        public string SignUpMethod(Member mb)
+        {
+
+            mb.userLevel = 3;
+            mb.totalStarRate = 0;
+            if (mb.city == null)
+            {
+                return "輸入東西";
+            }
+            new maimaiRepository<Member>().Create(mb);
+
+            return  "註冊成功" ; /* RedirectToAction("Homepage", "HomePage_C");*/
+        }
+
+
+
+
+
+
+
+
+
+
+        //public ActionResult test(Member mb)
+        //{
+        //    //if (ModelState.IsValid == false)
+        //    //{
+
+
+        //    //    return View();
+        //    //}
+        //    new maimaiRepository<Member>().Create(mb);
+
+        //    return View(); /* RedirectToAction("Homepage", "HomePage_C");*/
+        //}
+        public ActionResult fuck()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+      
+        public string CheckAccount(string account)
+        {
+            if (account == "")
+            {
+                return"2";
+            }
+
+
+
+            SqlConnection conn = new SqlConnection("data source=maimai.database.windows.net;initial catalog=maimai2;persist security info=True;user id=team4;password=MaiMai123;");
+            conn.Open();
+
+            SqlCommand bb = new SqlCommand("Select * From Member Where userAccount='" + account + "'", conn);
+            SqlDataReader ha = bb.ExecuteReader();
+            if (ha.Read())
+                 
+            {
+                return "1";
+            }
+            return "0";
+        }
+
+
+         
+       
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String LoginMethod(LoginViewModel login)
+        {
+            //if (ModelState.IsValid)
+            //{
+            //    return View();
+            //}
+
+            Member mb = db.Member.FirstOrDefault(m => m.userAccount == login.userAccount && m.userPassWord == login.userPassWord);
+            
+            if (mb == null)
+            {
+                 
+               
+                return "帳號不存在或密碼錯誤!";
+            }
+
+            Response.Cookies["LoginAccount"].Value = mb.UserID.ToString();
+            Response.Cookies["LoginName"].Value = mb.userAccount.ToString();
+            Response.Cookies["LoginID"].Value = mb.UserID.ToString();
+            Response.Cookies["MemberLevel"].Value = mb.userLevel.ToString();
+
+
+            if (login.RememberMe == "on")
+            {
+                Response.Cookies["MemberLevel"].Expires = DateTime.Now.AddDays(7);
+                Response.Cookies["LoginAccount"].Expires = DateTime.Now.AddDays(7);
+                Response.Cookies["LoginName"].Expires = DateTime.Now.AddDays(7);
+                Response.Cookies["LoginID"].Value = mb.UserID.ToString();
+            }
+
+
+
+            //return RedirectToAction("Homepage", "HomePage_C");
+            //return RedirectToAction("index", "Home");
+            return "登入成功";
+        
+        }
+        public ActionResult Logout()
+        {
+            Response.Cookies["LoginAccount"].Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies["LoginName"].Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies["MemberLevel"].Expires = DateTime.Now.AddDays(-1);
+            
+            return RedirectToAction("MaimaiIndexNew", "NewMaimaiIndex");
+        }
+
+        //public string uploadPhoto(upLoadPhotoViewModel data)
+        //{
+        //    if (data.upphoto == null)
+        //    {
+        //        return "../Content/resource_nico/images/無圖示.jpg";
+        //    }
+        //    //HttpPostedFileBase photo = new HttpPostedFileBase(upphoto);
+        //    string filename = data.upphoto.FileName;
+        //    data.upphoto.SaveAs(Server.MapPath("../Content/MemberImg/") + filename);
+        //    string filePath = $"../Content/MemberImg/{filename}";
+
+        //    return filePath;
+        //}
+
+    }
+}
